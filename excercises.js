c4a296787f5566155690b727b4e4ca7b3b724180
mayayr115 <mayaramkishuncsh2027@gmail.com>
2025-06-05 22:45:26 -0400

    Stopped at Ex 5 in JS

---
+++ excercises.js
@@ -0,0 +1,19 @@
+// Exercise 2
+console.log('Hello World');
+
+// Exercise 3
+const pi = 3.14159;
+const z ='Z';
+
+// Exercise 4
+const day_of_year = (x) => {
+  const date = x
+  console.log(`Day ${date.split(' ')[1]}`)
+};
+
+day_of_year('July 6')
+
+// Exercise 5
+for (let i = 0; i <= 5; i++) {
+  console.log(i)
+};
